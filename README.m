cc9d0aef906e57650b6e569c8df7880d7005beea
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-19 10:20:02 -0700

    Setting config

--- README.m
+++ README.m
@@ -1,52 +1,28 @@
 Setup
 %%% START %%%
 % Brandon Fong 
-% CES 440 Project 1
-% Due Date: Monday, 09/28/2020 
+% CES 440 Project 2
+% Due Date: Monday, 10/26/2020 
 % Name & ID : Brandon Fong (007808236)
 % email: fong.m.brandon97@gmail.com & fongb@sonoma.edu
 % Link to Report: NA
 % Link to Git: NA
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Problem 1: Fourier series of a square wave.
-% M-file name: src\Prob1\ProblemOne.m
-% Notes: 
-%       - In the script you will see that a square wave is the sum of a sine function for each odd increment
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-ProblemOne
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Problem 2: Fourier Series for an impulse train
-% M-file name: src\Prob1\ProblemTwo.m
+% Part 1:
+% M-file name: /src/Part1.m
 % Notes: 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-ProblemTwo
+Part1
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Problem 3: Generate a sinc function in Matlab.
-% M-file name: src\Prob1\ProblemThree.m
+% Part 2:
+% M-file name: /src/Part2.m
 % Notes: 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-ProblemThree
+Part1
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Problem 4: Rectangular function and its Fourier Transform
-% M-file name: src\Prob1\ProblemFour.m
-% Notes: 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-ProblemFour
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Problem 5: Time Scaling property of the fourier transform 
-% M-file name: src\Prob1\ProblemFive.m
-% Notes: 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-ProblemFive
 
 %%% END %%%
