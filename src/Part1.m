a617952986d4a33f62a1c1c2ddb1bfd709b54f46
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-20 09:10:06 -0700

    Setting part 1

--- src/Part1.m
+++ src/Part1.m
@@ -1,4 +1,10 @@
 % ltspice http://jeastham.blogspot.com/2009/10/adding-lm741-op-amp-model-to-ltspice.html
 % Bode plot demo 
-H = tf([1 0.1 7.5],[1 0.12 9 0 0]);
+R = const.Part1.Res;
+C = eval(const.Part1.Cap);
+
+coef1 = -(R*C)^2;
+coef2 = sqrt(2)*R*C;
+
+H = tf(1,[coef1 coef2 1])
 bode(H)
