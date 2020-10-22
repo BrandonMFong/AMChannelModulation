c8c23429bf5748902cbf69049cd9579112f3f20e
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-22 10:13:51 -0700

    Filtering out the higher channel

--- src/Part2.m
+++ src/Part2.m
@@ -58,7 +58,7 @@ soundsc(x4,8000)
 
 % b
 h=fir1(40,0.2);
-figure;freqz(h)
+% figure;freqz(h)
 
 % c 
 x5=filter(h,1,x4);
@@ -72,6 +72,10 @@ title('2.2.c');
 soundsc(x5,8000)
 
 % 2.3
+LowPass=fir1(40,0.3,'low'); % use this to filter the first channel 
+% figure;freqz(LowPass)
+HighPass=fir1(40,0.3,'high'); % use this to filter the first channel 
+figure;freqz(HighPass)
 
 load proj2_wave2 
 x = double(x)/32768;
@@ -90,10 +94,20 @@ title('2.3 X');
 % Channel two: 0.40
 
 % b
-y1=cos(2*pi*0.2*(1:length(x))).*x;
+channel1=cos(2*pi*0.2*(1:length(x))).*x;
+y1=filter(LowPass,1,channel1);
 Y1 = fft(y1);
 n = (length(Y1)-1)/2;
 f = (-n:n)/n/2;
-subplot(2,2,2);
+subplot(2,2,3);
 plot(f,fftshift(abs(Y1)));
-title('2.3.b.y1');
+title('2.3.b Channel 1 (y1)');
+
+channel2=cos(2*pi*0.2*(1:length(x))).*x;
+y2=filter(HighPass,1,channel2);
+Y2 = fft(y2);
+n = (length(Y2)-1)/2;
+f = (-n:n)/n/2;
+subplot(2,2,4);
+plot(f,fftshift(abs(Y2)));
+title('2.3.b Channel 1 (y2)');
