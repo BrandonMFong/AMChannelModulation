b1ccf972c86a8180fb8405f73527e3d0719b0a19
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-26 17:00:38 -0700

    clean to turnin

--- src/Part1.m
+++ src/Part1.m
@@ -1,5 +1,7 @@
 % ltspice http://jeastham.blogspot.com/2009/10/adding-lm741-op-amp-model-to-ltspice.html
-w = 1000:100:1000000; % define the frequency range
+w = 1000:1:1000000; % define the frequency range
+RC=10000*(2.5*10^(-9));
+% H = 1./(1 + (w.*(1j*sqrt(2)*RC)) - (w.*((RC)^2)));
 H = ButterworthTF(w);
 H0 = abs(H);
 theta0 = angle(H);
@@ -7,7 +9,6 @@ subplot(211), semilogx(w,20*log10(H0))
 subplot(212), semilogx(w,theta0)
 title('1')
 
-figure
 HMag = ButterMag(w);
 H0Mag = abs(HMag);
 theta0Mag = angle(HMag);
